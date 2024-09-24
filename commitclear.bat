c6ade67ba3772eafc70168f09b57f5bcb0445426
lling <www.1642760071@foxmail.com>
2024-09-24 14:01:18 +0800

    remove useless files

---
+++ commitclear.bat
@@ -0,0 +1,16 @@
+@echo off
+cd D:
+cd %~dp0
+rmdir /s /q %~dp0\simulator\win32\Resources
+rmdir /s /q %~dp0\assistserver\Debug
+rmdir /s /q %~dp0\assistserver\assistserver\Debug
+rmdir /s /q %~dp0\assistserver\vcpkg_installed
+rmdir /s /q %~dp0\assistserver\.vs
+rmdir /s /q %~dp0\frameworks\cocos2d-x\cocos\2d\Debug.win32
+rmdir /s /q %~dp0\frameworks\cocos2d-x\cocos\editor-support\spine\proj.win32\Debug.win32
+rmdir /s /q %~dp0\frameworks\cocos2d-x\cocos\scripting\lua-bindings\proj.win32\Debug.win32
+rmdir /s /q %~dp0\frameworks\cocos2d-x\external\recast\proj.win32\Debug.win32
+rmdir /s /q %~dp0\frameworks\cocos2d-x\tools\simulator\libsimulator\proj.win32\Debug.win32
+rmdir /s /q %~dp0\frameworks\runtime-src\proj.win32\.vs
+rmdir /s /q %~dp0\frameworks\runtime-src\proj.win32\Debug.win32
+echo. & pause
